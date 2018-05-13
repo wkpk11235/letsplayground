a08936d7f19825fcf706d837b87b3785011e1124
MatheMagika <wkpk11235@gmail.com>
2018-05-13 19:15:58 +0900

    UHHHH
    This is hard work.
    Pure hard work.
    I translated PyGame to JavaScript.
    So hard.
    So if you want to use it....
    
    GO AHEAD!!!! LOL

--- script.js
+++ script.js
@@ -1,80 +1,174 @@
-function logMouseMove(event) {
-  x=event.clientX
-  y=event.clientY
-  create_circle()
+canvas=document.getElementById('canvas');
+screen=canvas.getContext('2d');
+
+explosion_power=50;
+gravconst=0.05;
+alloted_space=50;
+fadespeed=0.7;
+
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+scrwidth=canvas.width;
+scrheight=canvas.height;
+
+
+sound=new Audio('explosion.wav');
+
+presses=[];
+fireworks=[];
+
+class powder {
+  constructor(x,y,dx=0,dy=0,radius=2,light=5,func=null) {
+    this.x=x;
+    this.y=y;
+    this.dx=dx;
+    this.dy=dy;
+    this.light=light;
+    this.r=Math.floor(Math.random()*256);
+    this.g=Math.floor(Math.random()*256);
+    this.b=Math.floor(Math.random()*256);
+    this.alpha=255;
+    this.radius=radius;
+    this.temp=[];
+    if (func==null){
+      this.haveFunc=false;
+    }
+    else {
+      this.haveFunc=true;
+      this.func=func;
+      this.extravars={};
+    }
+    fireworks.push(this);
+  }
+  destroy(){
+    fireworks.splice(fireworks.indexOf(this),1);
+  }
+  sim(){
+    this.dy-=gravconst/this.light;
+    this.y-=this.dy;
+    this.x+=this.dx;
+    this.temp=[
+      (this.r*this.alpha/255),
+      (this.g*this.alpha/255),
+      (this.b*this.alpha/255)
+    ];
+    screen.beginPath();
+    screen.fillStyle='rgb('+ this.temp.join(',') +')';
+    screen.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    screen.fill();
+    this.alpha-=fadespeed;
+    if (this.haveFunc) {
+      this.func(this);
+    }
+    if (this.x>scrwidth+alloted_space || this.x<-alloted_space || this.y>scrheight+alloted_space || this.y<-alloted_space || this.alpha<=0) {
+        fireworks.splice(fireworks.indexOf(this),1);
+    }
+  }
+}
+
+function boom1(dis){
+  for (i=0;i<explosion_power;++i){
+    new powder(dis.x,dis.y,Math.random()*4-1.5,Math.random()*4-1.5)
+  }
+  sound.cloneNode(true).play();
+  dis.destroy();
+}
+
+function boom2(dis){
+  new powder(dis.x,dis.y);
+}
+
+function boom1_but_wait(dis){
+  a=dis.extravars['timer'] || 0;
+  console.log(a);
+  a+=1;
+  if (a==200){
+    for (i=0;i<explosion_power;++i){
+      new powder(dis.x,dis.y,Math.random()*4-1.5,Math.random()*4-1.5);
+    }
+    dis.destroy();
+    sound.cloneNode(true).play();
+  }
+  dis.extravars['timer']=a;
+}
+
+function boom3(dis){
+  for (i=0;i<2;++i){
+    new powder(dis.x,dis.y,-1+i*2,2,5,5,boom1_but_wait);
+  }
+  dis.destroy();
+  sound.play();
+}
+
+explosions=[boom1,boom2,boom3];
+
+firework_radius=5;
+firework_speed=3;
+
+class firework {
+  constructor(x,y,func=null) {
+    this.x=Math.floor(Math.random()*(scrwidth+1));
+    this.y=scrheight;
+    this.dest_x=x;
+    this.dest_y=y;
+    this.color=[Math.floor(Math.random()*256),
+      Math.floor(Math.random()*256),
+      Math.floor(Math.random()*256),
+    ];
+    this.colorstring='rgb('+ this.color.join(',') +')';
+    this.angle=Math.atan2(this.dest_y-this.y,this.dest_x-this.x);
+    this.dx=Math.cos(this.angle)*firework_speed;
+    this.dy=Math.sin(this.angle)*firework_speed;
+    if (func==null){
+      this.func=explosions[Math.floor(Math.random() * explosions.length)];
+    }
+    else{
+      this.func=func;
+    }
+    fireworks.push(this);
+  }
+  destroy(){
+    fireworks.splice(fireworks.indexOf(this),1);
+  }
+  sim(){
+    this.x+=this.dx;
+    this.y+=this.dy;
+    if (this.y-this.dest_y<=0){
+      this.func(this);
+    }
+    if (this.x>scrwidth+alloted_space || this.x<-alloted_space || this.y>scrheight+alloted_space || this.y<-alloted_space) {
+      fireworks.splice(fireworks.indexOf(this),1);
+    }
+    else{
+      screen.beginPath();
+      screen.fillStyle=this.colorstring;
+      screen.arc(this.x, this.y, firework_radius, 0, 2 * Math.PI, false);
+      screen.fill();
+    }
+  }
+}
+
+function handle_all(){
+  for (i=0;i<fireworks.length;++i){
+    fireworks[i].sim();
+  }
 }
 
 function onTouch(event){
-  x=event.touches[0].pageX
-  y=event.touches[0].pageY
-  create_circle()
+  new firework(event.touches[0].pageX,event.touches[0].pageY)
+}
+
+function onClick(event){
+  new firework(event.clientX,event.clientY)
 }
 
 document.addEventListener('touchstart', onTouch)
-document.addEventListener('click', logMouseMove)
+document.addEventListener('click', onClick)
 
-canvas=document.getElementById('canvas')
-screen=canvas.getContext("2d")
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-
-x=canvas.width+1
-y=canvas.height+1
-
-width=10
-
-screen.fillStyle="white"
-
-circles=[]
-
-function circle(xpos,ypos,rad,dx=0,dy=0) {
-  this.x=xpos
-  this.y=ypos
-  this.radius=rad
-  this.dy=0
-  this.dx=0
-  this.draw=draw
-  this.simulate=simulate
-  circles.push(this);
+function Update(){
+  screen.clearRect(0,0,scrwidth,scrheight);
+  handle_all();
 }
 
-bounce_power=0.5
-
-function draw(){
-  screen.beginPath()
-  screen.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false)
-  screen.fill()
-}
-function simulate(){
-  if (this.y<canvas.height-50){
-    this.dy+=0.05
-  } else {
-    this.y=canvas.height-50
-    this.dy=-this.dy*bounce_power
-    this.radius=this.radius*bounce_power
-  }
-  this.x+=this.dx
-  this.y+=this.dy
-}
-
-certified_radius=0.1
-
-function simall(){
-  for (i=0;i<circles.length;++i){
-    circles[i].draw()
-    circles[i].simulate()
-  }
-}
-
-
-function simulation(){
-  screen.clearRect(0,0,canvas.width,canvas.height)
-  simall()
-
-}
-
-function create_circle(){
-  new circle(x,y,4)
-}
-
-setInterval(simulation,1)
+setInterval(Update,1);
